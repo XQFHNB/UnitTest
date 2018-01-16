152482e2b1f04bc39867fa0f6df96438285659d3
XQFHNB <1443114135@qq.com>
2018-01-17 00:50:24 +0800

    2 把launch界面调整了一下

--- src/com/xqf/test/launch/MyLaunch.java
+++ src/com/xqf/test/launch/MyLaunch.java
@@ -1,11 +1,7 @@
 package com.xqf.test.launch;
 
+import com.xqf.test.MyCalculatorAdvanced;
 import com.xqf.test.MyCalculatorSimple;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * 用户界面
@@ -14,22 +10,10 @@ import java.awt.event.ActionListener;
 public class MyLaunch {
     public static void main(String[] args) {
 
-        MyCalculatorSimple layoutSimple = new MyCalculatorSimple("XQF");
 
-        Frame frame = new Frame("请选择");
-        JButton buttonSimple = new JButton("Simple");
-        buttonSimple.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                layoutSimple.showCalculator();
-            }
-        });
-        JButton buttonAdvanced = new JButton("Advanced");
-        buttonAdvanced.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
+        MyCalculatorSimple myCalculatorSimple = new MyCalculatorSimple("XQF Simple");
+        myCalculatorSimple.showCalculator();
+        MyCalculatorAdvanced myCalculatorAdvanced = new MyCalculatorAdvanced("XQF Advanced");
+        myCalculatorAdvanced.showCalculator();
     }
 }
